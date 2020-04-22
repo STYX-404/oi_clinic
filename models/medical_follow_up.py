154aa987e19636332757bd73a81be66c5e6be804
nesmafayed <nesma.fayed98@gmail.com>
2020-04-22 23:52:51 +0200

    modified medical follow up model

--- models/medical_follow_up.py
+++ models/medical_follow_up.py
@@ -6,12 +6,19 @@ class StudentsFollowUp(models.Model):
     _name = 'medicalfollowup.data'
     _description = 'This model is used for medical follow-up for students, professors and employees'
 
-    stu_ids = fields.Many2many(comodel_name="students.data", column1="st_code", column2="st_name", string="Student Information", )
-    diagnosis = fields.Text(string="Diagnosis", required=True, )
-    doctor_id = fields.Many2one(comodel_name="doctors.data", string="Doctor", required=True, )
-    nurse_id = fields.Many2one(comodel_name="doctors.data", string="Nurse", required=True, )
-    em_ids = fields.Many2many(comodel_name="employees.data", column1="e_name", column2="e_code", string="Doctor Or Employee Information", )
-    date = fields.Date(string="Date", default=fields.Date.today(), )
+    stu_ids = fields.Many2many(comodel_name="students.data", column1="st_name", column2="st_code", string="Student", )
+    stu_nurse = fields.Many2one(comodel_name="doctors.data", string="Nurse", required=True, )
+    stu_doctor = fields.Many2one(comodel_name="doctors.data", string="Doctor", required=True, )
+    stu_diagnosis = fields.Text(string="Diagnosis", required=True, )
+    stu_date = fields.Date(string="Date", required=fields.Date.today(), )
+    em_ids = fields.Many2many(comodel_name="employees.data", column1="e_name", column2="e_code", string="Employee", )
+    em_doctor = fields.Many2one(comodel_name="doctors.data", string="Doctor", required=True, )
+    em_diagnosis = fields.Text(string="Diagnosis", required=True, )
+    em_date = fields.Date(string="Date", required=fields.Date.today(), )
+
+
+
+
 
 
 
