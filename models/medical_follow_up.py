db7155c5d55f8958164c713c9b06854ff920c2ae
nesmafayed <nesma.fayed98@gmail.com>
2020-04-23 09:20:17 +0200

    added new fields to Health Report Model

--- models/medical_follow_up.py
+++ models/medical_follow_up.py
@@ -3,22 +3,24 @@ from odoo import models, api, fields
 
 
 class StudentsFollowUp(models.Model):
-    _name = 'medicalfollowup.data'
-    _description = 'This model is used for medical follow-up for students, professors and employees'
+    _name = 'stmedicalfollowup.data'
+    _description = 'This model is used for medical follow-up for students'
 
-    stu_ids = fields.Many2many(comodel_name="students.data", column1="st_name", column2="st_code", string="Student", )
+    stu_id = fields.Many2one(comodel_name="students.data", string="Student", required=True,)
     stu_nurse = fields.Many2one(comodel_name="doctors.data", string="Nurse", required=True, )
     stu_doctor = fields.Many2one(comodel_name="doctors.data", string="Doctor", required=True, )
     stu_diagnosis = fields.Text(string="Diagnosis", required=True, )
     stu_date = fields.Date(string="Date", required=fields.Date.today(), )
-    em_ids = fields.Many2many(comodel_name="employees.data", column1="e_name", column2="e_code", string="Employee", )
+
+
+
+class EmployeeFollowUp(models.Model):
+    _name = 'emmedicalfollowup.data'
+    _description = 'This model is used for medical follow-up for professors and employees'
+
+    em_id = fields.Many2one(comodel_name="employees.data", string="Employee", required=True )
     em_doctor = fields.Many2one(comodel_name="doctors.data", string="Doctor", required=True, )
     em_diagnosis = fields.Text(string="Diagnosis", required=True, )
     em_date = fields.Date(string="Date", required=fields.Date.today(), )
-
-
-
-
-
 
 
--- models/st_health_report.py
+++ models/st_health_report.py
@@ -1,4 +1,6 @@
+
 from odoo import api, fields, models
+
 
 class HealthReport(models.Model):
     _name = 'health.report'
@@ -6,32 +8,77 @@ class HealthReport(models.Model):
 
     st_ids = fields.Many2many(comodel_name="students.data", column1="st_name", string="Student", )
 
-    he_value = fields.Float(string="Hemoglobin - Hb %", required=True, )
+    st_height = fields.Float(string="Height",  required=True, )
+    st_weight = fields.Float(string="Weight",  required=True, )
+    st_bmi = fields.Float(string="BMI",  required=False, compute="gen_bmi", )
+
+
+    right_eye = fields.Char(string="Right", required=True, )
+    left_eye = fields.Char(string="Left", required=True, )
+    glasses = fields.Boolean(string="Has Glasses ?",  )
+
+
+    breast_diseases = fields.Char(string="Breast Diseases", )
+    blood_pressure = fields.Char(string="Blood Pressure",  required=True,)
+    birth_defect = fields.Char(string="Birth Defect and Impairments", )
+
+
+    drugs = fields.Boolean(string="Drug Analysis",  )
+    result = fields.Selection(string="Result", selection=[('p', 'Positive'), ('n', 'Negative'), ], required=False, default='n', )
+    type_drug = fields.Char(string="Type of Drugs", required=False, )
+
+
+    st_notes = fields.Text(string="Notes", required=False, )
+
+
+    accept_unaccept = fields.Selection(string="Acceptable or Not", selection=[('ac', 'Acceptable'), ('un', 'Unacceptable'), ], required=True, default='un',)
+
+
+    lab_info = fields.Many2one(comodel_name="lab.reports", string="Laboratory",  )
+    he_value = fields.Float(string="Hemoglobin - Hb %",)
     he_normal_value = fields.Text(string="Normal Value", readonly=True, default="(12-16) mg/dl", )
 
-    cl_value = fields.Float(string="Control", required=True, )
+    cl_value = fields.Float(string="Control", )
     cl_normal_value = fields.Text(string="Normal Value", readonly=True, default="12", )
 
-    pt_value = fields.Float(string="Patient Time", required=True, )
+    pt_value = fields.Float(string="Patient Time", )
     pt_normal_value = fields.Text(string="Normal Value", readonly=True, default="(12-16) Seconds", )
 
-    co_value = fields.Float(string="Concentration", required=True, )
+    co_value = fields.Float(string="Concentration", )
     co_normal_value = fields.Text(string="Normal Value", readonly=True, default="(70-100) %", )
 
-    inr_value = fields.Float(string="INR", required=True, )
+    inr_value = fields.Float(string="INR", )
     inr_normal_value = fields.Text(string="Normal Value", readonly=True, default="(1-2)", )
 
-    rbs_value = fields.Float(string="Random Blood Sugar", required=True, )
+    rbs_value = fields.Float(string="Random Blood Sugar", )
     rbs_normal_value = fields.Text(string="Normal Value", readonly=True, default="(60-160) mg/dl", )
 
-    sc_value = fields.Float(string="S.Creatinine", required=True, )
+    sc_value = fields.Float(string="S.Creatinine", )
     sc_normal_value = fields.Text(string="Normal Value", readonly=True, default="(0.6-1.5) mg/dl", )
 
-    ur_value = fields.Float(string="Urea", required=True, )
+    ur_value = fields.Float(string="Urea", )
     ur_normal_value = fields.Text(string="Normal Value", readonly=True, default="(15-45) mg/dl", )
 
-    sg_value = fields.Float(string="S.GOT", required=True, )
+    sg_value = fields.Float(string="S.GOT", )
     sg_normal_value = fields.Text(string="Normal Value", readonly=True, default="(5-45) U/I", )
 
-    sgp_value = fields.Float(string="S.GPT", required=True, )
+    sgp_value = fields.Float(string="S.GPT", )
     sgp_normal_value = fields.Text(string="Normal Value", readonly=True, default="(5-45) U/I", )
+
+    # ------------------- generate BMI ---------------------
+
+    @api.multi
+    @api.depends('st_weight', 'st_height')
+    def gen_bmi(self):
+        for record in self:
+
+            if record.st_height >= 0.1:
+                weight = record.st_weight
+                height = record.st_height
+                height_m = height / 100
+                square_height = height_m * height_m
+                bmi = weight / square_height
+                record.st_bmi = bmi
+            else:
+                record.st_bmi = 0.0
+
